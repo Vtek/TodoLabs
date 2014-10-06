34de6061d4b5fcaacf82fc2a9059abf05448f1b0
pontoreau <pontoreau.sylvain@gmail.com>
2014-10-06 22:04:31 +0200

    Apply Blog modification to deal with toto models

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -1,24 +1,13 @@
-/**
- * Todo
- *
- * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
-  attributes: {
-    name : 'string',
-    age : {
-        type : 'integer',
-        required : true
-    },
-    email : {
-        type : 'email',
-        max : 150,
-        required : true
+    attributes : {
+        title : {
+            type : 'string',
+            required : true,
+            unique : true
+        },
+        done : {
+            type : 'boolean',
+            defaultsTo : false
+        }
     }
-  }
-
 };
