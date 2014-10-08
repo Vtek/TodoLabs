256687cc8baef6b9162ac2aee02879c04bb26f7d
pontoreau <pontoreau.sylvain@gmail.com>
2014-10-08 20:45:43 +0200

    Add priority property

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -5,6 +5,10 @@ module.exports = {
             required : true,
             unique : true
         },
+        priority : {
+            type : 'integer',
+            required : true
+        },
         done : {
             type : 'boolean',
             defaultsTo : false
